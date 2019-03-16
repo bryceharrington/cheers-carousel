9e436cedac86411b3c1f94e431fad4df99cdb2c6
Bryce Harrington <bryce@bryceharrington.org>
2019-03-15 19:26:42 -0700

    Import example carousel demo code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+    render() {
+        return (
+        <div id="cheers-carousel" className="carousel slide carousel-fade carousel-fullscreen" data-ride="carousel">
+          <ol className="carousel-indicators">
+            <li data-target="#carousel-example-generic2" data-slide-to="0" className="active"></li>
+            <li data-target="#carousel-example-generic2" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic2" data-slide-to="2"></li>
+          </ol>
+
+          <div className="carousel-inner" role="listbox">
+            <div className="carousel-item active"
+                 style={{border: "20px solid black", "background-image": "url('img/carousel1.jpg')"}}>
+              <div className="overlay"></div>
+              <div className="carousel-caption">
+                <h1 className="display-2 font-weight-bold">Default text 1</h1>
+                <p className="h4 font-weight-light">detail</p>
+              </div>
+            </div>
+            <div className="carousel-item"
+                 style={{border: "20px solid black", "background-image": "url('img/carousel2.jpg')"}}>
+              <div className="overlay"></div>
+              <div className="carousel-caption">
+                <h1 className="display-2 font-weight-bold">Default text 2</h1>
+                <p className="h4 font-weight-light">detail</p>
+              </div>
+            </div>
+            <div className="carousel-item"
+                 style={{border: "20px solid black", "background-image": "url('img/carousel3.jpg')"}}>
+              <div className="overlay"></div>
+              <div className="carousel-caption">
+                <h1 className="display-2 font-weight-bold">Default text 3</h1>
+                <p className="h4 font-weight-light">detail</p>
+              </div>
+            </div>
+          </div>
+
+          <a className="carousel-control-prev"
+             href="#carousel-example-generic2"
+             role="button"
+             data-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
           </a>
-        </header>
-      </div>
-    );
-  }
+          <a className="carousel-control-next"
+             href="#carousel-example-generic2"
+             role="button"
+             data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
+        );
+    }
 }
 
 export default App;
